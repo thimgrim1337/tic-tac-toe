5c27b633f2b66c6c6807c5d95d0c938f98d9646a
thimgrim1337 <11806430+thimgrim1337@users.noreply.github.com>
2023-01-31 19:03:15 +0100

    Add checkWinner function

---
+++ main.js
@@ -0,0 +1,74 @@
+'use strict';
+const Player = (playerChoice) => {
+  const sign = playerChoice;
+
+  return { sign };
+};
+
+const player = Player('X');
+
+const game = (() => {
+  const gameboard = new Array(9);
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [3, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const updateGameboard = (index, sign) => {
+    gameboard[index] = sign;
+  };
+
+  const checkWinner = () => {
+    for (let i = 0; i < winConditions.length; i++) {
+      if (
+        gameboard[winConditions[i][0]] != undefined &&
+        gameboard[winConditions[i][1]] != undefined &&
+        gameboard[winConditions[i][2]] != undefined
+      ) {
+        if (
+          gameboard[winConditions[i][0]] === gameboard[winConditions[i][1]] &&
+          gameboard[winConditions[i][1]] === gameboard[winConditions[i][2]]
+        ) {
+          console.log(`${player.sign} win`);
+        }
+      }
+    }
+  };
+
+  return { gameboard, updateGameboard, checkWinner };
+})();
+
+const displayController = (() => {
+  const fields = document.querySelectorAll('.field');
+  const result = document.querySelector('.result');
+  const turn = document.querySelector('.turn');
+  const resetBtn = document.querySelector('.reset-btn');
+
+  function startGame() {
+    fields.forEach((field) => field.addEventListener('click', fieldClicked));
+    resetBtn.addEventListener('click', restartGame);
+  }
+
+  function fieldClicked(e) {
+    e.target.textContent = player.sign;
+    e.target.removeEventListener('click', fieldClicked);
+    game.updateGameboard(e.target.dataset.index, e.target.textContent);
+    game.checkWinner();
+    changeSign();
+  }
+
+  function changeSign() {
+    player.sign === 'X' ? (player.sign = 'O') : (player.sign = 'X');
+    turn.textContent = `${player.sign}'s turn`;
+  }
+
+  function restartGame() {}
+
+  startGame();
+})();
